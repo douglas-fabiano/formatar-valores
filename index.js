39ccfa0fe35ff830bed8c7a1ace9e6da745c23fb
Douglas Fabiano Batista <douglas.fabianob@gmail.com>
2019-01-09 10:22:09 -0400

    Adicionado condições para não tentar formatar valores null e underfined

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
 module.exports = {
   cpfCnpj (cpfCnpj) {
-    if (cpfCnpj.length === 11) {
-      return cpfCnpj.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, '$1.$2.$3-$4')
-    } else if (cpfCnpj.length === 14) {
-      return cpfCnpj.replace(/(\d{2})(\d{3})(\d{3})(\d{4})(\d{2})/, '$1.$2.$3/$4-$5')
+    if (cpfCnpj !== null && cpfCnpj !== undefined && cpfCnpj.length > 0) {
+      if (cpfCnpj.length === 11) {
+        return cpfCnpj.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, '$1.$2.$3-$4')
+      } else if (cpfCnpj.length === 14) {
+        return cpfCnpj.replace(/(\d{2})(\d{3})(\d{3})(\d{4})(\d{2})/, '$1.$2.$3/$4-$5')
+      }
+    } else {
+      return cpfCnpj
     }
   },
   cep (cep) {
-    return cep.replace(/(\d{2})(\d{3})(\d{3})/, '$1.$2-$3')
+    if (cep !== null && cep !== undefined && cep.length > 0) {
+      return cep.replace(/(\d{2})(\d{3})(\d{3})/, '$1.$2-$3')
+    } else {
+      return cep
+    }
   },
   moeda (moeda, casasDecimais) {
-    return moeda.toLocaleString('pt-BR', { minimumFractionDigits: casasDecimais })
+    if (moeda !== null && moeda !== undefined) {
+      return moeda.toLocaleString('pt-BR', { minimumFractionDigits: casasDecimais })
+    } else {
+      return moeda
+    }
   }
 }
