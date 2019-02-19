08b954e9588180a9fb5c7bae8badef8876428f86
Douglas Fabiano Batista <douglas.fabianob@gmail.com>
2019-02-19 13:40:03 -0400

    Adicionado ESLint ao projeto

--- src/telefone.js
+++ src/telefone.js
@@ -1,6 +1,6 @@
 export default (valor) => {
   if (valor !== null && valor !== undefined && valor.length > 0) {
-    return valor.replace(/(\d)(\d{4})$/,"$1-$2")
+    return valor.replace(/(\d)(\d{4})$/, '$1-$2')
   } else {
     return valor
   }
